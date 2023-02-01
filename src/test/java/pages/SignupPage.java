57d082c791c63af7715cf05bd713741f70eb3b31
Nemanja154 <nemanja154@gmail.com>
2023-02-01 23:14:52 +0100

    LoginTest finished. SigupTest finished.

--- src/test/java/pages/SignupPage.java
+++ src/test/java/pages/SignupPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import com.github.javafaker.Faker;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,8 +8,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SignupPage extends BasePage {
 
+    Faker faker = new Faker();
+
     @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/a[4]")
-    private WebElement signupButton;
+    private WebElement enterSignupScreen;
+
+    @FindBy (id = "name")
+    private WebElement name;
 
     @FindBy(id = "email")
     private WebElement emailSignUpField;
@@ -19,13 +25,22 @@ public class SignupPage extends BasePage {
     @FindBy(id = "confirmPassword")
     private WebElement confirmPasswordSignUpField;
 
+    @FindBy (xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div[5]/button")
+    private WebElement signUpButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[3]/div/div/div/div/div[1]")
+    private WebElement emailAlreadyExistError;
+
+    @FindBy (xpath = "//*[@id=\"app\"]/div[4]/div/div/div[1]")
+    private WebElement verifyMessage;
+
 
     public SignupPage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
 
     public void visitTheSignUpPage() {
-        signupButton.click();
+        enterSignupScreen.click();
     }
 
     public String getEmailSignUpFieldType() {
@@ -38,5 +53,29 @@ public class SignupPage extends BasePage {
 
     public String getConfirmPasswordSignUpFieldType() {
         return confirmPasswordSignUpField.getAttribute("type");
+    }
+
+    public void existingUserSignup(){
+        this.name.sendKeys("Test Test");
+        this.emailSignUpField.sendKeys("admin@admin.com");
+        this.passwordSignUpField.sendKeys("123654");
+        this.confirmPasswordSignUpField.sendKeys("123654");
+        this.signUpButton.click();
+    }
+
+    public boolean isErrorEmailDisplayed() {
+        return emailAlreadyExistError.isDisplayed();
+    }
+
+    public boolean isVerifyMessageDisplayed(){
+        return verifyMessage.isDisplayed();
+    }
+
+    public void validSignUp (){
+        this.name.sendKeys(faker.name().fullName());
+        this.emailSignUpField.sendKeys(faker.internet().emailAddress());
+        this.passwordSignUpField.sendKeys("milan1234");
+        this.confirmPasswordSignUpField.sendKeys("milan1234");
+        this.signUpButton.click();
     }
 }
--- src/test/java/tests/SignupTest.java
+++ src/test/java/tests/SignupTest.java
@@ -1,10 +1,13 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class SignupTest extends BaseTest{
+public class SignupTest extends BaseTest {
 
     @BeforeMethod
     @Override
@@ -31,5 +34,21 @@ public class SignupTest extends BaseTest{
         Assert.assertEquals(emailTypeActual, emailTypeExpected);
         Assert.assertEquals(passwordTypeActual, passwordTypeExpected);
         Assert.assertEquals(confirmPasswordTypeActual, confirmPasswordTypeExpected);
+    }
+
+    @Test
+    public void userAlreadyExist() {
+        signupPage.existingUserSignup();
+        Assert.assertTrue(signupPage.isErrorEmailDisplayed());
+        Assert.assertTrue(driver.getCurrentUrl().contains("/signup"));
+    }
+
+
+    //problemi. ovde sam stao
+    @Test
+    public void isVerifyMessageDisplayedTest() {
+        signupPage.validSignUp();
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[4]/div/div/div[1]")));
+        Assert.assertTrue(signupPage.isVerifyMessageDisplayed());
     }
 }
